f724023915b486bc28ed9d8a5005c9e1dce02605
José Luis Tello <jltello96@gmail.com>
2021-09-29 03:18:06 -0500

    adds

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
+from base.models import Task
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def taskList(request):
-    return HttpResponse('To Do List')
+class TaskList(ListView):
+    model = Task
