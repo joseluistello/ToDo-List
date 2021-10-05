cfb3d6298bbc941c5a711e9d225a6667d23e5540
José Luis Tello <jltello96@gmail.com>
2021-10-05 00:26:40 -0500

    adds

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -13,3 +14,10 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+### Crea todo el modelo en un objeto nuevo
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
