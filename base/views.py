1513daf2197a3c90bbb9dc50789d23cfa9ff60e2
José Luis Tello <jltello96@gmail.com>
2021-10-07 18:46:45 -0500

    lOGIN

--- base/views.py
+++ base/views.py
@@ -36,6 +36,11 @@ class RegisterPage(FormView):
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
 
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 
 ### List
 
